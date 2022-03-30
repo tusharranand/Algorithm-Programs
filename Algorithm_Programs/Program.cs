f66a3441b1298a1f4d689bc9396a1c8155c86626
Tushar Anand <tusharranand@gmail.com>
2022-03-30 17:51:56 +0530

    Introduction

---
+++ Algorithm_Programs/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Programs
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs");
+            Console.WriteLine("Enter a number to view respective program: ");
+            Console.WriteLine("1 for String Permutations");
+            Console.WriteLine("2 for Binary Search a Word");
+            Console.WriteLine("3 for Insertion Sort");
+            Console.WriteLine("4 for Bubble Sort");
+            Console.WriteLine("5 for Merge Sort");
+            Console.WriteLine("6 for Anagram Detection");
+            Console.WriteLine("7 for Prime Numbers");
+            Console.WriteLine("8 for Extention of above programs");
+            Console.WriteLine("9 for Rewrite Search and Sort Algorithms using Generics");
+            Console.WriteLine("10 for Find your Number");
+            Console.WriteLine("11 for Task Completion");
+            Console.WriteLine("12 for Customize Message Demonstration");
+            int Control = Convert.ToInt32(Console.ReadLine());
+
+            switch (Control)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+        }
+    }
+}
